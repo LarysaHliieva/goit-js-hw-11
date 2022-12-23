2ba36998b136b7e016293883bb274146bbc67dae
LarysaHliieva <lora.glievaya@gmail.com>
2022-12-23 15:30:18 +0200

    update loadmorebtn

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,10 +1,18 @@
 const KEY_API = '19043103-0cd62514f089da7e89200caeb';
 const BASE_URL = 'https://pixabay.com/api';
 
-export function fetchImages(query) {
-  return fetch(
-    `${BASE_URL}/?key=${KEY_API}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
+export function fetchImages(query, page) {
+  const searchParams = new URLSearchParams({
+    q: query,
+    key: KEY_API,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 4,
+    page,
+  });
+
+  return fetch(`${BASE_URL}/?${searchParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -8,28 +8,56 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
+let page = 1;
+let searchQuery = '';
+
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
+  loadMoreHide();
 
-  const searchQuery = e.currentTarget.elements.searchQuery.value;
-  console.log(searchQuery);
+  searchQuery = e.currentTarget.elements.searchQuery.value;
+  resetPage();
 
-  fetchImages(searchQuery)
+  fetchImages(searchQuery, page)
     .then(images => {
       if (images.hits.length === 0) {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
+        clearImagesMarkup();
+        return;
       }
 
       appendImagesMarkup(images.hits);
+      loadMoreShow();
     })
     .catch();
 }
 
+function onLoadMore(searchQuery) {
+  loadMoreDisasbled();
+  fetchImages(searchQuery, page)
+    .then(images => {
+      appendImagesMarkup(images.hits);
+      incrementPage();
+    })
+    .catch()
+    .finally(() => loadMoreAnabled());
+}
+
+function incrementPage() {
+  page += 1;
+}
+
+function resetPage() {
+  page = 1;
+}
+
 function appendImagesMarkup(images) {
+  // refs.gallery.innerHTML = createImagesMarkup(images);
   refs.gallery.insertAdjacentHTML('beforeend', createImagesMarkup(images));
 }
 
@@ -69,4 +97,24 @@ function createImagesMarkup(images) {
   `
     )
     .join('');
+}
+
+function clearImagesMarkup() {
+  refs.gallery.innerHTML = '';
+}
+
+function loadMoreShow() {
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+
+function loadMoreHide() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+
+function loadMoreDisasbled() {
+  refs.loadMoreBtn.disabled = true;
+}
+
+function loadMoreAnabled() {
+  refs.loadMoreBtn.disabled = true;
 }
